acf4d604ce8b8fec5e400ff1d9e6907d1bac8e93
Maker0N <maker0n@DESKTOP-60U3U24.localdomain>
2021-09-23 17:40:07 +0500

    0.9.1

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Square from './square.jsx'
 
 const Container = () => {
-  const squaresArr = Array(836).fill(<Square />)
+  const squaresArr = Array(1596).fill(<Square />)
   return (
     <div className="container">{squaresArr.map((it) => it)}</div>
   )
